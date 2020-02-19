df2a1820cd04423523cf9cc29e0a0314a4a6427f
Neal Sidhwaney <username@users.noreply.github.com>
2020-02-18 19:43:15 -0800

    Add thread & frame information, and resume VM after receiving class prepare event

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 import com.sun.jdi.Bootstrap;
 import com.sun.jdi.AbsentInformationException;
 import com.sun.jdi.VirtualMachineManager;
+import com.sun.jdi.IncompatibleThreadStateException;
 import com.sun.jdi.VirtualMachine;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -25,23 +26,24 @@ public class Main {
     Thread debugeeOutputHandler;
 
     public static void main(String[] args) {
-        new Main().run();
+        new Main().run(args[0]);
     }
 
-    private void run() {
+    private void run(String mainClass) {
         VirtualMachineManager vmm = Bootstrap.virtualMachineManager();
         var connector = vmm.launchingConnectors().stream().filter(x -> x.name() == "com.sun.jdi.CommandLineLaunch").findAny().get();
         out.println(connector);
         var connArgs = connector.defaultArguments();
-        connArgs.get("main").setValue("Fibonacci");
+        connArgs.get("main").setValue(mainClass);
         out.println(connArgs);
         try {
             vm = connector.launch(connArgs);
+            //            vm.setDebugTraceMode(VirtualMachine.TRACE_ALL);
             vm.eventRequestManager().createClassPrepareRequest().enable();
-            debugeeOutputHandler = new Thread(new DebuggeeOutputHandler(vm.process()));
-            debugeeOutputHandler.start();
             startEventProcessor();
             out.println("started event processor");
+            debugeeOutputHandler = new Thread(new DebuggeeOutputHandler(vm.process()));
+            debugeeOutputHandler.start();
             while (true) {
                 out.print("cmd> ");
                 String input = readUntilNewline();
@@ -76,8 +78,9 @@ public class Main {
                         } else if (event instanceof ClassPrepareEvent) {
                             ClassPrepareEvent cpe = (ClassPrepareEvent)(event);
                             out.println("Class prepare: " + cpe.referenceType().name());
+                            vm.resume();
                         } else {
-                            out.print(event);
+                            out.println(event);
                         }
                     }
                 }
@@ -111,6 +114,19 @@ public class Main {
                     x.allMethods().forEach(y -> out.println("\t" + y));
                 });
         }
+
+        if (input.equals("threads")) {
+            vm.allThreads().forEach(x -> {
+                    out.println(x);
+                    try {
+                        x.frames().forEach(y -> {
+                                out.println("\t" + y + "\t" + y.location());
+                            });
+                    } catch (IncompatibleThreadStateException itse) {
+                        out.println("\t" + "No frame information available(itse)");
+                    }
+                });
+        }
     }
 }
 
@@ -125,9 +141,14 @@ class DebuggeeOutputHandler implements Runnable {
 
     public void run() {
         char ch;
+        int readVal;
         try {
-            while((ch = (char)processOutputStream.read()) != -1) {
+            readVal = processOutputStream.read();
+
+            while(readVal != -1) {
+                ch = (char) readVal;
                 out.print(ch);
+                readVal = processOutputStream.read();
             }
         } catch (IOException ioe) {
             out.println(ioe);
